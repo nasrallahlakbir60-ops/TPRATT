68f2527d5abdc06b18c508703111826177101f22
nasrallahlakbir60-ops <nasrallahlakbir60@gmail.com>
2026-05-16 16:15:34 +0100

    4

--- index.js
+++ index.js
@@ -10,12 +10,39 @@ console.log(password);
 console.log(submit);
 
 submit.addEventListener("click", function(event) {
+    event.preventDefault();
 
-    if (nom.value === "" || email.value === "" || password.value === "") {
+    if (nom.value === "") {
+        alert("Please enter your name.");
+        return;
+    }
+    else if (email.value === "") {
+        alert("Please enter your email.");
+        return;
+    }
+    else if (password.value === "") {
+        alert("Please enter your password.");
+        return;
+    }
+    else if (nom.value === "" && email.value === "" && password.value === "") {
         alert("Please fill in all fields.");
         return;
     }
-    event.preventDefault();
+   if (password.value.length < 8) {
+    alert("Le mot de passe doit contenir au moins 8 caractères.");
+    return;
+}
+
+if (
+    !/[A-Z]/.test(password.value) ||      !/[a-z]/.test(password.value) ||      !/[0-9]/.test(password.value) ||      !/[!@#$%^&*(),.?":{}|<>]/.test(password.value) 
+) {
+    alert("Le mot de passe doit contenir une majuscule, une minuscule, un chiffre et un symbole.");
+    return;
+}
+    if (!email.value.includes("@")) {
+        alert("Please enter a valid email address.");
+        return;
+    }
     console.log("button clicked");
     console.log(nom.value);
     console.log(email.value);
